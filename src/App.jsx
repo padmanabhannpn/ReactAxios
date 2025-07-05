431bed94bdaece3a83364eaf260fa4ae01523d52
Padmanabhan N <padmanabhan@Padmanabhans-MacBook-Pro.local>
2025-07-05 09:54:14 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,11 @@
 import { useEffect, useState } from 'react'
 
 import './App.css'
-import axios from 'axios'
+
 import { callProduct, callSingleProduct } from './Service/allAPI'
 
 function App() {
  
-
-
-//   // Make a request for a user with a given ID
-axios.get('https://fakestoreapi.com/products')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  });
-
-
-  axios.get('https://fakestoreapi.com/products/1')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-
-
-  
 
 
 useEffect(() => {
